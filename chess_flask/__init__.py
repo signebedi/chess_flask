0c0d6450e89dfa9a2cc592cbf4f8275223beecab
signebedi <signe@siftingwinnowing.com>
2023-09-25 16:56:33 -0500

    Added: basic data model (#3)

--- chess_flask/__init__.py
+++ chess_flask/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import logging
+import logging, chess
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chess.db'
@@ -8,3 +8,15 @@ db = SQLAlchemy(app)
 
 # Setup logging
 logging.basicConfig(filename='chess.log', level=logging.INFO)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    turn = db.Column(db.String(1))  # 'w' or 'b'
+    moves = db.relationship('Move', backref='game')
+
+class Move(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uci = db.Column(db.String(10))
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+
+db.create_all()
